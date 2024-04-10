0f3f84b9509dfc7ea9508f629d377a1a07ae7f66
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-10 22:51:05 +0530

    added errors in async await

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,17 +1,26 @@
-console.log('d')
+console.log("d");
 
-console.log('v')
+console.log("v");
 
-const getTodos = async ()=>{
-    const response = await fetch("/todos/todos.json")
-    const data =  await response.json()
-    console.log(data)
-
-}
+const getTodos = async () => {
+  const response = await fetch("/todos/todoss.json");
+  if(response.status===200){
+    const data = await response.json();
+    return data;
+  }
+  else{
+    throw new Error('Cannot fetch')
+  } 
+  
+};
 
 getTodos()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {console.log(err.message)});
 
-console.log('d')
+console.log("d");
 // fetch("/todos/todos.json").then((response)=>{
 //     console.log(response)
 //     return response.json()
