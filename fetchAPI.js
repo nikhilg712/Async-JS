df09b00736323caf163699c5221ab43723b0482f
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-10 22:43:53 +0530

    Added Async Await

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,8 +1,22 @@
-fetch("/todos/todos.json").then((response)=>{
-    console.log(response)
-    return response.json()
-}).then((data)=>{
+console.log('d')
+
+console.log('v')
+
+const getTodos = async ()=>{
+    const response = await fetch("/todos/todos.json")
+    const data =  await response.json()
     console.log(data)
-}).catch((err)=>{
-    console.log(err)
-})
+
+}
+
+getTodos()
+
+console.log('d')
+// fetch("/todos/todos.json").then((response)=>{
+//     console.log(response)
+//     return response.json()
+// }).then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log(err)
+// })
